3e5914484878fc0ef42a6bce21672c8922c1d6c4
Keerthana-2276 <keerthanas.27csa@licet.ac.in>
2026-04-27 00:45:11 +0530

    Update Hellocontroller.java

--- src/main/java/com/example/demo/Hellocontroller.java
+++ src/main/java/com/example/demo/Hellocontroller.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class Hellocontroller {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World   hi!");
+        System.out.println("Hello, World   hii!");
     }
 }
